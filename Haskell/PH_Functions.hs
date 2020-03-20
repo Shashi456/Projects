bb3c20b07da3911c6725b9ab960ac106860e70c0
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2020-03-20 12:31:46 +0530

    Programming haskell chapter2

--- Haskell/PH_Functions.hs
+++ Haskell/PH_Functions.hs
@@ -1,12 +1,15 @@
+main = print(f 3)
+
 -- A function in haskell is  a mapping taking one or more arguments and producing 
 -- a single result. { The following doesnt compile yet }.
 -- Another interpretation of this as given in the textbook is that, first 
 -- the argument replaces the arguments in the function , and then the plus sign is 
 -- applied . 
 double x = x + x 
-double 3 -- Output will be 6  
+-- double 3 
+-- Output will be 6  
 -- Functions can also be passed to other functions 
-double (double x) -- is a valid expression 
+-- double (double x) -- is a valid expression 
 quadruple x = double (double x) -- is also a valid function, here 
 -- quadruple has an argument x and then is passed to double 
 -- which is then passed to double, Interesting to think of it as function coupling.
@@ -19,16 +22,17 @@ quadruple x = double (double x) -- is also a valid function, here
 -- Applications in lambda calculus are left associative. 
 
 
-sum [1..n] -- is the coupling of two functions where in [1..n] returns a list of numbers 
+-- sum [1..n] -- is the coupling of two functions where in [1..n] returns a list of numbers 
 -- from 1 to n and sum reduces this list. This can again be seen at the earlier thought,
 -- function applied to arguments, sum function is being applied to the list argument. 
 
 -- Breaking down the sum function and writing it on my own. 
 -- First the base case 
-sum [] = 0 ; sum (n:ns) = n + sum ns -- Has to be written in same line. 
+suml [] = 0 ; suml (n:ns) = n + suml ns -- Has to be written in same line. 
 -- The way you can think about it is as follows, sum [] = 0 is the base case of when the list is empty, while in sum (n:ns), represents the list addition. Think of Ns as a list of number. So first take one n from the ns, and then add it, and then sum the remaining ns. 
 -- Similarly writing the factorial function you get: 
-fact 1 = 1; fact x = x * fact (x-1)
+fact 1 = 1; fact x = x * fact (x-1) 
+fact n = product [1..n] -- alternative representation 
 
 
 -- Sum has the type Num a => [a] -> a 
@@ -42,3 +46,30 @@ quicksort (x:xs) =
         biggerSorted = quicksort [a | a <- xs, a > x]  
     in  smallerSorted ++ [x] ++ biggerSorted 
 -- The indentation is sorta annoying but mandatory. 
+
+
+-- Product function 
+prod (x:xs) = x * prod xs; prod [] = 1
+
+
+-- reverse (x:xs) = reverse xs + x , Doesn't work , Need to find a way to take from the tail of a distribution
+
+
+mean ns = sum ns `div` length ns -- write div in invert single quotes between two arguments, or can also be written as div (sum ns) (length ns)
+
+
+-- The following is called the layout structure and is a good and intutive way to define functions. 
+f x = a + x + b 
+    where
+        a = 1
+        b = 2
+
+
+--definition of last in other terms 
+last (x:xs) = head( reverse (x:xs))
+-- last [1..10] returns 10
+
+--definition of init in two possible ways. init returns the list by dropping the last element.
+init1 (x:xs) = take (length (x:xs) - 1) (x:xs)
+init2 (x:xs) = reverse( drop 1 (reverse (x:xs)))
+
--- Haskell/Sorting.hs
+++ Haskell/Sorting.hs
@@ -1,9 +1,19 @@
     
---main = print(quicksort [1, 3, 2, 1.5, 8, 7, 5 ]
-main =  quicksort ["xsbac", "siand", "aooke"] -- Doesn't work, how to input strings?
+main = print(quicksortd [1, 3, 2, 1.5, 8, 7, 5])
+-- main =  quicksort ["xsbac", "siand", "aooke"] -- Doesn't work, how to input strings?
     
-quicksort [] = []  
+quicksortd [] = []  
+quicksortd (x:xs) =  
+    let smallerSorted = quicksortd [a | a <- xs, a <= x]  -- from all xs, 
+        biggerSorted = quicksortd [a | a <- xs, a > x]  
+    in  biggerSorted ++ [x] ++ smallerSorted
+
+quicksort [] = [] 
 quicksort (x:xs) =  
-    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  -- from all xs, 
         biggerSorted = quicksort [a | a <- xs, a > x]  
     in  smallerSorted ++ [x] ++ biggerSorted 
+
+
+
+    
